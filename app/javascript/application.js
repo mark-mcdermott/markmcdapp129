f72251abca22af05f286e9ac2a0c69c61cbcb791
Mark McDermott <mark@markmcdermott.io>
2022-05-22 23:57:09 -0500

    api

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,27 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import jquery from 'jquery'
 
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
 	return new bootstrap.Popover(popoverTriggerEl)
 })
+
+window.jQuery = jquery
+window.$ = jquery
+$('#someDiv').css('border','1px solid red')
+
+const url = '/scores/api';
+
+$.ajax({
+	url: url,
+	type: "POST",
+	beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+	data: {
+	score: {
+		"val": 1015, 
+	}
+},
+	success: function(resp){ json: 'SUCCESS WOO' }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  resources :scores
 
   root 'home#index'
-
-
-
+  post 'scores/api', to: 'scores#api_create'
 
 end
